3c10945a1d36551aa74dab04fb196bc7917009cc
hdmun <huidae.mun@gmail.com>
2022-04-04 18:11:53 +0900

    비주얼 스튜디오 솔루션, 프로젝트 생성

---
+++ PokerServer/PokerServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokerServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
