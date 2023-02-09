6b6ffee48702e2d9d35d0062c297abf0e7028b25
hdmun <huidae.mun@gmail.com>
2023-02-09 10:49:06 +0900

    [REFACTOR] `Enum.GetValues` 중복 호출 제거

--- Domain.PokerRule.Data/Card.cs
+++ Domain.PokerRule.Data/Card.cs
@@ -6,17 +6,20 @@ namespace Domain.PokerRule.Data
     public class Card
     {
         public static Card Get(Rank rank, Shape shape)
-            => Cards[((int)rank)][(int)shape];
+            => Cards[((int)rank) + ((int)shape * RankCount)];
 
         private static Card Of(Rank rank, Shape shape)
             => new Card(rank, shape);
 
-        private static Card[][] Cards = Enum.GetValues<Rank>()
-            .Select((rank)
-                => Enum.GetValues<Shape>()
-                    .Select((shape) => Of(rank, shape))
-                    .ToArray())
+        public readonly static int RankCount = Enum.GetValues<Rank>().Length;
+
+        public readonly static Card[] Cards = Enum.GetValues<Shape>()
+            .SelectMany((shape)
+                => Enum.GetValues<Rank>()
+                    .Select((rank) => Of(rank, shape)))
             .ToArray();
+
+        public static int DeckCount = Cards.Length;
 
         private readonly Rank _rank;
         private readonly Shape _shape;
@@ -30,7 +33,7 @@ namespace Domain.PokerRule.Data
         public Rank Rank { get => _rank; }
         public Shape Shape { get => _shape; }
 
-        public int ToMaskIndex() => (int)_rank + (int)_shape * Enum.GetValues(typeof(Rank)).Length;
+        public int ToMaskIndex() => (int)_rank + (int)_shape * RankCount;
 
         public ulong ToBitMask() => 1UL << ToMaskIndex();
     }
