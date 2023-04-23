5728a793ec5fa38c462763ae76cea990c28b2633
hdmun <hdmun@kakao.com>
2023-04-23 16:41:00 +0900

    [FEAT] `ForEach` 확장 메소드 추가

---
+++ Domain.PokerRule.Tests/Extentions/EnumerableExtentionsTests.cs
@@ -0,0 +1,34 @@
+﻿using Domain.PokerRule.Extentions;
+using System.Linq;
+using Xunit;
+
+namespace Domain.PokerRule.Tests.Extentions
+{
+    public class EnumerableExtentionsTests
+    {
+        [Fact]
+        public void ForEachFullyTest()
+        {
+            var array = new[] { 1, 2, 3, 4, 5 };
+
+            var enumerable = array.Where(_ => true).ForEachFully(2);
+            using (var enumertor = enumerable.GetEnumerator())
+            {
+                enumertor.MoveNext();
+                Assert.Equal(3, enumertor.Current);
+
+                enumertor.MoveNext();
+                Assert.Equal(4, enumertor.Current);
+
+                enumertor.MoveNext();
+                Assert.Equal(5, enumertor.Current);
+
+                enumertor.MoveNext();
+                Assert.Equal(1, enumertor.Current);
+
+                enumertor.MoveNext();
+                Assert.Equal(2, enumertor.Current);
+            }
+        }
+    }
+}
---
+++ Domain.PokerRule/Extentions/EnumerableExtentions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Domain.PokerRule.Extentions
+{
+    public static class EnumerableExtentions
+    {
+        public static IEnumerable<T> ForEachFully<T>(this IEnumerable<T> source, int start)
+        {
+            var skipped = source.Skip(start);
+            using (var e = skipped.GetEnumerator())
+            {
+                while (e.MoveNext())
+                    yield return e.Current;
+            }
+
+            var take = source.Take(start);
+            using (var e = take.GetEnumerator())
+            {
+                while (e.MoveNext())
+                    yield return e.Current;
+            }
+        }
+    }
+}
